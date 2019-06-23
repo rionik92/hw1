2b90cf0f421aa0758379eba33e87e50a7c0408b1
Roman <rionik@mail.ru>
2019-06-23 19:55:18 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+
+        int myNumber = 45;
+        System.out.println(myNumber);
+
+        String name = "Roma";
+        String surname = "Nikolaev";
+        System.out.println(name + " " + surname);
+
+        final String hello = "Привет!";
+        System.out.println(hello);
+
+
+    }
+}
+
+
+
+
